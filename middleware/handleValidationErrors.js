a429d3aa2f71411c1b5d1c026bcd6599482245ee
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-06 14:34:28 +0300

    take the view so validation can happen on all pages

--- middleware/handleValidationErrors.js
+++ middleware/handleValidationErrors.js
@@ -1,19 +1,19 @@
 const { validationResult } = require("express-validator");
 
-module.exports = (req, res, next) => {
-  const errors = validationResult(req);
+const handleValidationErrors = (view, extraData = {}) => {
+  return (req, res, next) => {
+    const errors = validationResult(req);
 
-  if (errors.isEmpty()) {
-    return next();
-  }
+    if (errors.isEmpty()) {
+      return next();
+    }
 
-  return res
-    .status(400)
-    .render(
-      req.originalUrl.includes("register") ? "auth/register" : "auth/login",
-      {
-        errors: errors.array(),
-        oldInput: req.body,
-      },
-    );
+    return res.status(400).render(view, {
+      errors: errors.array(),
+      oldInput: req.body,
+      ...extraData,
+    });
+  };
 };
+
+module.exports = handleValidationErrors;
