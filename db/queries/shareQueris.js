ae683ae20f848bb44327fc7f1bd2e7713ef80191
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-06 14:44:55 +0300

    Add share crud queries to support file sharing

---
+++ db/queries/shareQueris.js
@@ -0,0 +1,80 @@
+const prisma = require("../../config/prisma");
+
+// =====================================
+// CREATE / UPDATE
+// =====================================
+
+const upsertShareLinkQuery = async (folderId, expiresAt) => {
+  return prisma.shareLink.upsert({
+    where: {
+      folderId,
+    },
+
+    update: {
+      expiresAt,
+    },
+
+    create: {
+      folderId,
+      expiresAt,
+    },
+
+    include: {
+      folder: true,
+    },
+  });
+};
+
+// =====================================
+// READ
+// =====================================
+
+const getShareLinkQuery = async (id) => {
+  return prisma.shareLink.findUnique({
+    where: {
+      id,
+    },
+
+    include: {
+      folder: {
+        include: {
+          children: true,
+          files: true,
+        },
+      },
+    },
+  });
+};
+
+// =====================================
+// DELETE
+// =====================================
+
+const deleteShareLinkQuery = async (folderId) => {
+  return prisma.shareLink.deleteMany({
+    where: {
+      folderId,
+    },
+  });
+};
+
+// =====================================
+// CHECKUP
+// =====================================
+const shareExistsQuery = async (folderId) => {
+  return prisma.shareLink.findUnique({
+    where: {
+      folderId,
+    },
+  });
+};
+
+module.exports = {
+  upsertShareLinkQuery,
+
+  getShareLinkQuery,
+
+  deleteShareLinkQuery,
+
+  shareExistsQuery,
+};
