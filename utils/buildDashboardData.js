01c4d2bd00e0a112d273cec86f562069f5121815
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-08 13:13:28 +0300

    Fetch root files as well as root folders in a promise

--- utils/buildDashboardData.js
+++ utils/buildDashboardData.js
@@ -1,4 +1,5 @@
 const folderQueries = require("../db/queries/folderQueries");
+const fileQueries = require("../db/queries/fileQueries");
 
 // =====================================
 // HELPERS
@@ -37,8 +38,12 @@ const buildDashboardData = async (userId, folderId = null) => {
   // =================================
   // ROOT
   // =================================
-
   if (folderId === null) {
+    const [sidebarFolders, files] = await Promise.all([
+      folderQueries.getFolderTreeQuery(userId),
+      fileQueries.getRootFilesQuery(userId),
+    ]);
+
     return {
       title: "My Drive",
 
@@ -50,7 +55,7 @@ const buildDashboardData = async (userId, folderId = null) => {
 
       folders: sidebarFolders,
 
-      files: [],
+      files,
 
       errors: [],
       oldInput: {},
